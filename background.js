11b31d64a0ba9bcc27c39c36c3d64330cff6e6bc
DoviFeldman <96088906+DoviFeldman@users.noreply.github.com>
2025-05-14 01:11:01 -0400

    Create background.js

---
+++ background.js
@@ -0,0 +1,12 @@
+setInterval(function() {
+  fetch('http://192.168.0.7/status') // replace with your ESP32s home wifi IP address, its different for each wifi thing
+    .then(response => response.text())
+    .then(data => {
+      if (data === '1') {
+        chrome.tabs.create({ url: 'http://google.com/' });
+      } else if (data === '2') {
+        chrome.tabs.create({ url: 'https://chatgpt.com' });
+      }
+    })
+    .catch(error => console.log('Error:', error));
+}, 250);
